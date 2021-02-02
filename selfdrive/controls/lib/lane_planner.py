fdfc2a631f767bc48c825b325a0a12511ba9a4fb
Qiming Chen <simoncqm@gmail.com>
2021-02-02 14:42:22 -0800

    global CAMERA_OFFSET

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -21,6 +21,7 @@ class LanePlanner:
     params = Params()
     camera_offset_str = params.get("CameraOffset")
     if camera_offset_str is not None and camera_offset_str != "":
+      global CAMERA_OFFSET
       CAMERA_OFFSET = 0.01 * int(camera_offset_str)
       params.put("CameraOffset", camera_offset_str)
     self.ll_t = np.zeros((TRAJECTORY_SIZE,))
